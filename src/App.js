e9191ac57c0956d08573b2de8f0737f62266a0fd
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-10-26 23:48:40 +0200

    riordinato il codice e aggiunto il key attribute con id

--- src/App.js
+++ src/App.js
@@ -1,48 +1,9 @@
 // import Componente1 from './Componente1';
 
 import FirstCard from "./FirstCard";
+import Cards from "./Cards";
 
 // USO DELLE PROPS CON L AUSILIO DI ARRAY DI OBJECT JAVASCRIPT
-let Cards = [
-	{
-
-		nome: 'ROMA',
-		img: 'http://traveldigg.com/wp-content/uploads/2016/05/Colosseum-Photography.jpg',
-		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
-		
-	},
-	{
-	  
-		nome: 'MILANO',
-		img: 'https://images2-milano.corriereobjects.it/methode_image/2015/02/18/Milano/Foto%20Gallery/duomo_LI34321_2_MGZOOM.jpg',
-		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
-	  
-	},
-	{
-		nome: 'LAVELLO',
-		img: 'https://2.bp.blogspot.com/-KeUhV3cgpXM/VyJ01D6rSpI/AAAAAAAAj7c/yHLqwI-WGEMpaVGQQmnCoiNuSS4XezbBgCLcB/s1600/Lavello%2Bistituto%2Bcomprensivo%2B1%2BIMG_1443.jpg',
-		description: 'Lavello - Basilicata Turistica È uno dei comuni della splendida area del Vulture e in esso si concentrano diversi luoghi di interesse storico-artistico che testimoniano le sue antiche origini.'
-	},
-	{
-
-		nome: 'ROMA',
-		img: 'http://traveldigg.com/wp-content/uploads/2016/05/Colosseum-Photography.jpg',
-		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
-		
-	},
-	{
-	  
-		nome: 'MILANO',
-		img: 'https://images2-milano.corriereobjects.it/methode_image/2015/02/18/Milano/Foto%20Gallery/duomo_LI34321_2_MGZOOM.jpg',
-		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
-	  
-	},
-	{
-		nome: 'LAVELLO',
-		img: 'https://2.bp.blogspot.com/-KeUhV3cgpXM/VyJ01D6rSpI/AAAAAAAAj7c/yHLqwI-WGEMpaVGQQmnCoiNuSS4XezbBgCLcB/s1600/Lavello%2Bistituto%2Bcomprensivo%2B1%2BIMG_1443.jpg',
-		description: 'Lavello - Basilicata Turistica È uno dei comuni della splendida area del Vulture e in esso si concentrano diversi luoghi di interesse storico-artistico che testimoniano le sue antiche origini.'
-	},
-]
 
 
 
@@ -59,14 +20,17 @@ function App() {
         {/* <FirstCard {...PrimaCard}>
 			{/* INSERIAMO UN CHILDREN */}
 			{/* <a className={"BTN BTN-PRIMARY".toLocaleLowerCase()} href="www.getyourguide.it/Roma/Attività">SCPRI COME VENIRE A TROVARCI</a>
-		</FirstCard> */} */}
+		</FirstCard> */} 
         
 		{/* esempio di mappatura e scomposizione di un array */}
 		{/* <p className="text-success">{arr.map((el)=> <h2>{el}</h2>)}</p> */}
         
 		{/* mappo l'array di oggetti e returno la componente firstcard e con lo apread operator prendo le props quindi in parametri dell'array di oggetti */}
         {Cards.map((card)=>{
-			return <FirstCard {...card}/>
+			// metodo 1 per destrutturare la key attraverso l'id
+			// const {id} = card
+			// metodo 2
+			return <FirstCard key={card.id} {...card}/>
 		})}
       </div>
       
---
+++ src/Cards.js
@@ -0,0 +1,40 @@
+let Cards = [
+	{
+		id:1,
+		nome: 'ROMA',
+		img: 'http://traveldigg.com/wp-content/uploads/2016/05/Colosseum-Photography.jpg',
+		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
+		
+	},
+	{
+		id:2,
+		nome: 'MILANO',
+		img: 'https://images2-milano.corriereobjects.it/methode_image/2015/02/18/Milano/Foto%20Gallery/duomo_LI34321_2_MGZOOM.jpg',
+		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
+	  
+	},
+	
+	{
+		id:3,
+		nome: 'ROMA',
+		img: 'http://traveldigg.com/wp-content/uploads/2016/05/Colosseum-Photography.jpg',
+		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
+		
+	},
+	{
+		id:4,
+		nome: 'MILANO',
+		img: 'https://images2-milano.corriereobjects.it/methode_image/2015/02/18/Milano/Foto%20Gallery/duomo_LI34321_2_MGZOOM.jpg',
+		description: 'Cè tempo fino alle ore 12.00 del 2 novembre 2023 per presentare i progetti che verranno selezionati per gli spettacoli da realizzare nel Sistema Musei di Roma Capitale. Sarà infatti il 25 novembre l’appuntamento di quest’anno con la nuova edizione di Musei in Musica, uno dei fiori all’occhiello del programma culturale e di intrattenimento capitolino.'.toLocaleUpperCase()
+	  
+	},
+	{
+		id:5,
+		nome: 'LAVELLO',
+		img: 'https://2.bp.blogspot.com/-KeUhV3cgpXM/VyJ01D6rSpI/AAAAAAAAj7c/yHLqwI-WGEMpaVGQQmnCoiNuSS4XezbBgCLcB/s1600/Lavello%2Bistituto%2Bcomprensivo%2B1%2BIMG_1443.jpg',
+		description: 'Lavello - Basilicata Turistica È uno dei comuni della splendida area del Vulture e in esso si concentrano diversi luoghi di interesse storico-artistico che testimoniano le sue antiche origini.'
+	},
+]
+
+
+export default Cards;
